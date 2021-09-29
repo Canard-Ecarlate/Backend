2a91f8680c3d13e7720b5588d6649161db04afe6
Vincent Letourmy <letourmyvincent@outlook.fr>
2021-09-29 16:15:10 +0200

    getByName in userRepo

--- CanardEcarlate.Api/Startup.cs
+++ CanardEcarlate.Api/Startup.cs
@@ -50,7 +50,6 @@ namespace CanardEcarlate.Api
             services.AddSingleton<UserService>();
             services.AddSingleton<GameService>();
 
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
