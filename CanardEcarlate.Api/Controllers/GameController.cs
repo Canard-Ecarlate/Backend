26a34ef55de263a3fec3d0f9147d721d80bd0185
Vincent Letourmy <letourmyvincent@outlook.fr>
2021-12-02 12:56:05 +0100

    Groups

--- CanardEcarlate.Api/Controllers/GameController.cs
+++ CanardEcarlate.Api/Controllers/GameController.cs
@@ -1,6 +1,9 @@
+using System;
 using CanardEcarlate.Api.Models;
+using CanardEcarlate.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Configuration;
 
 namespace CanardEcarlate.Api.Controllers
 {
@@ -10,17 +13,19 @@ namespace CanardEcarlate.Api.Controllers
     {
         private readonly IHubContext<CanardEcarlateHub> _ceHub;
 
-        public GameController(IHubContext<CanardEcarlateHub> ceHub)
+        public GameController(IHubContext<CanardEcarlateHub> ceHub, IConfiguration configuration)
         {
             _ceHub = ceHub;
+            Configuration = configuration;
         }
-        
+        // Exemple de récupération des infos dans appSettings
+        public IConfiguration Configuration { get; }
+
         [HttpPost]
         public ActionResult<string> DrawCard(string userName)
         {
             // Exemple d'utilisation de signalR
             _ceHub.Clients.All.SendAsync("AfterDrawCard", "card drawn");
-            
             // If win => global stat nbWonAs....ByNbPlayers +1
             return new OkObjectResult("draw card in " + userName + " 's hand");
         }
