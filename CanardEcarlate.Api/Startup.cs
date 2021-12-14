dae90ef33590232ea1dd6f8d0f1c806ef45d29c3
Evi Raoul <eviraoul@gmail.com>
2021-12-14 14:30:42 +0100

    Https retiré

--- CanardEcarlate.Api/Startup.cs
+++ CanardEcarlate.Api/Startup.cs
@@ -66,7 +66,7 @@ namespace CanardEcarlate.Api
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CanardEcarlate.Api v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
             
