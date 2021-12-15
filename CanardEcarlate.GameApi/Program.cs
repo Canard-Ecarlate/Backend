cf3a775576889f74c0455d07ab881c3d26be1d98
Vincent Letourmy <letourmyvincent@outlook.fr>
2021-12-15 17:01:03 +0100

    correction Programs

--- CanardEcarlate.GameApi/Program.cs
+++ CanardEcarlate.GameApi/Program.cs
@@ -1,17 +1,21 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System.IO;
 
 namespace CanardEcarlate.GameApi
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
+            var root = Directory.GetCurrentDirectory();
+            var dotenv = Path.Combine(root, ".env");
+            DotEnv.Load(dotenv);
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
     }
 }
