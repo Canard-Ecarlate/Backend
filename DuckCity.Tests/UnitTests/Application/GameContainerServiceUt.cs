9c2ea7ff51d467665882406696d00008a0c93ca2
Vincent Letourmy <letourmyvincent@outlook.fr>
2022-02-16 10:28:22 +0100

    codacy

--- DuckCity.Tests/UnitTests/Application/GameContainerServiceUt.cs
+++ DuckCity.Tests/UnitTests/Application/GameContainerServiceUt.cs
@@ -1,18 +1,6 @@
 ﻿namespace DuckCity.Tests.UnitTests.Application;
 
 public class GameContainerServiceUt
-{/*
-    // Class to test
-    private readonly GameContainerService _gameContainerService;
-
-    // Mock
-    private readonly Mock<IUserRepository> _mockUserRep = new();
-    private readonly Mock<IRoomPreviewRepository> _mockRoomPreviewRep = new();
-    private readonly Mock<IGameContainerRepository> _mockGameContainerRep = new();
-
-    // Constructor
-    public GameContainerServiceUt()
-    {
-        _gameContainerService = new GameContainerService(_mockUserRep.Object, _mockRoomPreviewRep.Object, _mockGameContainerRep.Object);
-    }*/
+{
+    
 }
