f381a17cab140ed76c6cc3f7e1184b143011eee8
Surdidi <40174459+Surdidi@users.noreply.github.com>
2022-02-16 18:04:17 +0100

    #90 Tester les fonctionnalités de gestion de compte V1 (#137)
    * unit test
    
    * test unitaire app
    
    * unit test
    
    * test unitaire app
    
    * correct bug
    
    * code factor
    
    * erreur extremement grave corrigé
    
    * doublon cproj
    
    Co-authored-by: Dautheville Dylan <surdidi@gmail.com>

---
+++ DuckCity.Tests/UnitTests/Api/UserControllerUt.cs
@@ -0,0 +1,72 @@
+﻿using DuckCity.Api.Controllers;
+using DuckCity.Api.DTO.User;
+using DuckCity.Application.UserService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace DuckCity.Tests.UnitTests.Api
+{
+    public class UserControllerUt
+    {
+        // Class to test
+        private readonly UserController _userController;
+        // Mock
+        private readonly Mock<IUserService> _mockUserService = new();
+        // Constructor
+        public UserControllerUt()
+        {
+            _userController =
+                new UserController(_mockUserService.Object)
+                {
+                    ControllerContext = {HttpContext = new Mock<HttpContext>().Object}
+                };
+        }
+
+        /**
+     * Tests
+     */
+        [Theory]
+        [InlineData(ConstantTest.UserId, ConstantTest.String, ConstantTest.Email)]
+        public void DeleteUserTest(string id, string name, string mail)
+        {
+            //Given
+            UserDto userDto = new() { UserId = id,UserName = name,UserMail = mail};
+            _mockUserService.Setup(mock => mock.DeleteAccountUser(id));
+            
+            //When
+            ActionResult<string> actionResult = _userController.DeleteUser(userDto);
+            OkObjectResult? result = actionResult.Result as OkObjectResult;
+            
+            //Then
+            Assert.NotNull(result);
+            string res = (string) result?.Value!;
+            Assert.Equal("The account is deleted", res);
+            
+            //Verify
+            _mockUserService.Verify(mock => mock.DeleteAccountUser(id), Times.Once);
+        }
+        
+        [Theory]
+        [InlineData(ConstantTest.UserId, ConstantTest.String, ConstantTest.String,ConstantTest.String)]
+        public void ChangePasswordTest(string id, string actualPassword, string newPassword, string passwordConfirmation)
+        {
+            //Given
+            UserChangePassword userChangePassword = new() {UserId = id,ActualPassword = actualPassword,NewPassword = newPassword,PasswordConfirmation = passwordConfirmation};
+            _mockUserService.Setup(mock => mock.ChangePasswordUser(id,actualPassword,newPassword,passwordConfirmation));
+            
+            //When
+            ActionResult<string> actionResult = _userController.ChangePassword(userChangePassword);
+            OkObjectResult? result = actionResult.Result as OkObjectResult;
+
+            //Then
+            Assert.NotNull(result);
+            string res = (string) result?.Value!;
+            Assert.Equal("The password has been changed", res);
+            
+            //Verify
+            _mockUserService.Verify(mock => mock.ChangePasswordUser(id,actualPassword,newPassword,passwordConfirmation), Times.Once);
+        }
+    }
+}
