8c70a1b5c6ff08d9ab41240f9a4ab99c1783d3b8
Dautheville Dylan <surdidi@gmail.com>
2022-02-18 14:36:04 +0100

    Gestion salle avec code

--- DuckCity.Application/RoomPreviewService/RoomPreviewService.cs
+++ DuckCity.Application/RoomPreviewService/RoomPreviewService.cs
@@ -37,25 +37,4 @@ public class RoomPreviewService : IRoomPreviewService
     {
         _roomPreviewRepository.Delete(roomId);
     }
-    
-    public string GenerateCode()
-    {
-        const int lenghtCode = 4;
-        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        string code = new string(Enumerable.Repeat(chars, lenghtCode).Select(s => s[Random.Next(s.Length)]).ToArray());
-        if (!CodeIsValid(code))
-        {
-            return GenerateCode();
-        }
-        return code;
-    }
-
-    private bool CodeIsValid(string code)
-    {
-        if (_forbiddenWords.Contains(code))
-        {
-            return false;
-        }
-        return !_roomPreviewRepository.CodeIsExist(code);
-    }
 }
